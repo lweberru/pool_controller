8412fd8360f352fa7a2e36b084e60afba077bf9e
Lukas Weberruß <lukas@klue.de>
2026-01-04 17:55:10 +0000

    Fix: Tons of bugs

--- custom_components/pool_controller/config_flow.py
+++ custom_components/pool_controller/config_flow.py
@@ -5,98 +5,63 @@ from .const import *
 
 class PoolControllerConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
-
     def __init__(self):
         self.data = {}
 
     async def async_step_user(self, user_input=None):
-        """Schritt 1: Basis-Setup."""
         if user_input is not None:
             self.data.update(user_input)
             return await self.async_step_heating()
-
         return self.async_show_form(
             step_id="user",
             data_schema=vol.Schema({
                 vol.Required(CONF_POOL_NAME, default="Whirlpool"): str,
-                vol.Required(CONF_MAIN_SWITCH, default="switch.whirlpool"): selector.EntitySelector(
-                    selector.EntitySelectorConfig(domain="switch")
-                ),
+                vol.Required(CONF_MAIN_SWITCH, default="switch.whirlpool"): selector.EntitySelector(selector.EntitySelectorConfig(domain="switch")),
                 vol.Required(CONF_WATER_VOLUME, default=1000): vol.Coerce(int),
                 vol.Required(CONF_DEMO_MODE, default=False): bool,
             })
         )
 
     async def async_step_heating(self, user_input=None):
-        """Schritt 2: Heizung & Leistung."""
         if user_input is not None:
             self.data.update(user_input)
             return await self.async_step_water_quality()
-
         return self.async_show_form(
             step_id="heating",
             data_schema=vol.Schema({
-                vol.Required(CONF_TEMP_WATER, default="sensor.esp32_5_cd41d8_whirlpool_temperature"): selector.EntitySelector(
-                    selector.EntitySelectorConfig(domain="sensor", device_class="temperature")
-                ),
-                vol.Required(CONF_TEMP_OUTDOOR): selector.EntitySelector(
-                    selector.EntitySelectorConfig(domain="sensor", device_class="temperature")
-                ),
-                vol.Optional(CONF_AUX_HEATING_SWITCH, default="switch.whirlpool_heizung"): selector.EntitySelector(
-                    selector.EntitySelectorConfig(domain="switch")
-                ),
-                vol.Optional(CONF_MAIN_POWER_SENSOR, default="sensor.whirlpool_leistung"): selector.EntitySelector(
-                    selector.EntitySelectorConfig(domain="sensor", device_class="power")
-                ),
-                vol.Optional(CONF_AUX_POWER_SENSOR, default="sensor.whirlpool_heizung_leistung"): selector.EntitySelector(
-                    selector.EntitySelectorConfig(domain="sensor", device_class="power")
-                ),
+                vol.Required(CONF_TEMP_WATER, default="sensor.esp32_5_cd41d8_whirlpool_temperature"): selector.EntitySelector(selector.EntitySelectorConfig(domain="sensor", device_class="temperature")),
+                vol.Required(CONF_TEMP_OUTDOOR, default="sensor.hue_outdoor_motion_sensor_1_temperatur"): selector.EntitySelector(selector.EntitySelectorConfig(domain="sensor", device_class="temperature")),
+                vol.Optional(CONF_AUX_HEATING_SWITCH, default="switch.whirlpool_heizung"): selector.EntitySelector(selector.EntitySelectorConfig(domain="switch")),
+                vol.Optional(CONF_MAIN_POWER_SENSOR, default="sensor.whirlpool_leistung"): selector.EntitySelector(selector.EntitySelectorConfig(domain="sensor", device_class="power")),
+                vol.Optional(CONF_AUX_POWER_SENSOR, default="sensor.whirlpool_heizung_leistung"): selector.EntitySelector(selector.EntitySelectorConfig(domain="sensor", device_class="power")),
             })
         )
 
     async def async_step_water_quality(self, user_input=None):
-        """Schritt 3: Wasserqualität (ESP32 Sensoren)."""
         if user_input is not None:
             self.data.update(user_input)
             return await self.async_step_logic()
-
         return self.async_show_form(
             step_id="water_quality",
             data_schema=vol.Schema({
-                vol.Optional(CONF_PH_SENSOR, default="sensor.esp32_5_cd41d8_whirlpool_ph"): selector.EntitySelector(
-                    selector.EntitySelectorConfig(domain="sensor")
-                ),
-                vol.Optional(CONF_CHLORINE_SENSOR, default="sensor.esp32_5_cd41d8_whirlpool_chlor"): selector.EntitySelector(
-                    selector.EntitySelectorConfig(domain="sensor")
-                ),
-                vol.Optional(CONF_SALT_SENSOR, default="sensor.esp32_5_cd41d8_whirlpool_salt"): selector.EntitySelector(
-                    selector.EntitySelectorConfig(domain="sensor")
-                ),
-                vol.Optional(CONF_TDS_SENSOR, default="sensor.esp32_5_cd41d8_whirlpool_conductivity"): selector.EntitySelector(
-                    selector.EntitySelectorConfig(domain="sensor")
-                ),
+                vol.Optional(CONF_PH_SENSOR, default="sensor.esp32_5_cd41d8_whirlpool_ph"): selector.EntitySelector(selector.EntitySelectorConfig(domain="sensor")),
+                vol.Optional(CONF_CHLORINE_SENSOR, default="sensor.esp32_5_cd41d8_whirlpool_chlor"): selector.EntitySelector(selector.EntitySelectorConfig(domain="sensor")),
+                vol.Optional(CONF_SALT_SENSOR, default="sensor.esp32_5_cd41d8_whirlpool_salt"): selector.EntitySelector(selector.EntitySelectorConfig(domain="sensor")),
+                vol.Optional(CONF_TDS_SENSOR, default="sensor.esp32_5_cd41d8_whirlpool_conductivity"): selector.EntitySelector(selector.EntitySelectorConfig(domain="sensor")),
             })
         )
 
     async def async_step_logic(self, user_input=None):
-        """Schritt 4: Zeitplanung."""
         if user_input is not None:
             self.data.update(user_input)
             return self.async_create_entry(title=self.data[CONF_POOL_NAME], data=self.data)
-
         return self.async_show_form(
             step_id="logic",
             data_schema=vol.Schema({
-                vol.Optional(CONF_POOL_CALENDAR, default="calendar.whirlpool"): selector.EntitySelector(
-                    selector.EntitySelectorConfig(domain="calendar")
-                ),
-                vol.Optional(CONF_HOLIDAY_CALENDAR, default="calendar.deutschland_bw"): selector.EntitySelector(
-                    selector.EntitySelectorConfig(domain="calendar")
-                ),
-                vol.Optional(CONF_PV_SURPLUS_SENSOR): selector.EntitySelector(
-                    selector.EntitySelectorConfig(domain="sensor", device_class="power")
-                ),
+                vol.Optional(CONF_POOL_CALENDAR, default="calendar.whirlpool"): selector.EntitySelector(selector.EntitySelectorConfig(domain="calendar")),
+                vol.Optional(CONF_HOLIDAY_CALENDAR, default="calendar.deutschland_bw"): selector.EntitySelector(selector.EntitySelectorConfig(domain="calendar")),
+                vol.Optional(CONF_PV_SURPLUS_SENSOR): selector.EntitySelector(selector.EntitySelectorConfig(domain="sensor", device_class="power")),
                 vol.Required(CONF_QUIET_START, default="22:00"): str,
-                vol.Required(CONF_QUIET_END, default="06:00"): str,
+                vol.Required(CONF_QUIET_END, default="08:00"): str,
             })
         )
--- custom_components/pool_controller/coordinator.py
+++ custom_components/pool_controller/coordinator.py
@@ -15,13 +15,7 @@ class PoolControllerDataCoordinator(DataUpdateCoordinator):
 
     def __init__(self, hass: HomeAssistant, entry):
         self.entry = entry
-        super().__init__(
-            hass,
-            _LOGGER,
-            name=DOMAIN,
-            update_interval=timedelta(seconds=30),
-        )
-        
+        super().__init__(hass, _LOGGER, name=DOMAIN, update_interval=timedelta(seconds=30))
         self.target_temp = 38.0
         self.is_bathing = False
         self.last_filter_run = dt_util.now() - timedelta(hours=12)
@@ -31,8 +25,6 @@ class PoolControllerDataCoordinator(DataUpdateCoordinator):
     async def _async_update_data(self):
         try:
             now = dt_util.now()
-            
-            # 1. Daten abrufen
             water_temp = self._get_float_state(self.entry.data.get(CONF_TEMP_WATER))
             outdoor_temp = self._get_float_state(self.entry.data.get(CONF_TEMP_OUTDOOR))
             pv_surplus = self._get_float_state(self.entry.data.get(CONF_PV_SURPLUS_SENSOR))
@@ -41,131 +33,91 @@ class PoolControllerDataCoordinator(DataUpdateCoordinator):
             main_power = self._get_float_state(self.entry.data.get(CONF_MAIN_POWER_SENSOR)) or DEFAULT_MAIN_POWER
             aux_power = self._get_float_state(self.entry.data.get(CONF_AUX_POWER_SENSOR)) or DEFAULT_AUX_POWER
             
-            # 2. Berechnungen
-            tds_value = self.calculate_tds(conductivity)
+            tds_value = round(conductivity * 0.64) if conductivity else None
             is_holiday = await self._is_holiday()
-            is_quiet = self._check_quiet_time(is_holiday)
+            
+            # Neue differenzierte Ruhezeit-Prüfung
+            is_quiet = self._check_quiet_time_advanced(is_holiday)
             
             is_paused = self.pause_until is not None and now < self.pause_until
-            if is_paused and now >= self.pause_until:
-                self.pause_until = None
-                is_paused = False
-
-            # 3. Thermik
-            total_heating_power = main_power + aux_power
-            heat_up_time = self.calculate_heat_up_time(water_temp, total_heating_power)
+            heat_up_time = self.calculate_heat_up_time(water_temp, main_power + aux_power)
             frost_danger = outdoor_temp is not None and outdoor_temp < 3.0
             
-            # 4. Kalender & Filter
             preheat_needed = await self._check_calendar_preheat(heat_up_time)
-            filter_needed = self._check_filter_requirement()
+            filter_needed = (dt_util.now() - self.last_filter_run) > timedelta(hours=12)
 
-            # 5. Entscheidungs-Logik
-            should_main_on = (
-                frost_danger or 
-                (not is_paused and (preheat_needed or self.is_bathing or filter_needed or 
-                (pv_surplus is not None and pv_surplus > total_heating_power and water_temp < self.target_temp)))
-            )
-
+            should_main_on = (frost_danger or (not is_paused and (preheat_needed or self.is_bathing or filter_needed or (pv_surplus is not None and pv_surplus > (main_power + aux_power) and water_temp < self.target_temp))))
             if is_quiet and not frost_danger:
                 should_main_on = False
 
             should_aux_on = should_main_on and water_temp is not None and (self.target_temp - water_temp > 2.0)
-            
             if filter_needed and not preheat_needed and not self.is_bathing:
                 should_aux_on = False
 
             return {
-                "water_temp": water_temp,
-                "outdoor_temp": outdoor_temp,
-                "tds_value": tds_value,
-                "heat_up_time_mins": heat_up_time,
-                "frost_danger": frost_danger,
-                "is_quiet_time": is_quiet,
-                "is_paused": is_paused,
-                "should_main_on": should_main_on,
-                "should_aux_on": should_aux_on,
-                "demo_mode": self.demo_mode,
-                "is_holiday": is_holiday
+                "water_temp": water_temp, "outdoor_temp": outdoor_temp, "tds_value": tds_value,
+                "heat_up_time_mins": heat_up_time, "frost_danger": frost_danger,
+                "is_quiet_time": is_quiet, "is_paused": is_paused,
+                "should_main_on": should_main_on, "should_aux_on": should_aux_on,
+                "demo_mode": self.demo_mode, "is_holiday": is_holiday
             }
-
         except Exception as err:
             _LOGGER.error("Fehler im Pool-Coordinator: %s", err)
             raise UpdateFailed(f"Update fehlgeschlagen: {err}")
 
-    def calculate_tds(self, conductivity):
-        if conductivity is None: return None
-        return round(conductivity * 0.64)
+    def _check_quiet_time_advanced(self, is_holiday):
+        """Differenzierte Ruhezeiten für Wochentage vs. Wochenende/Feiertage."""
+        now = dt_util.now()
+        now_time = now.time()
+        is_weekend = now.weekday() >= 5 # 5=Samstag, 6=Sonntag
+        
+        # Bestimme aktive Zeitfenster
+        if is_weekend or is_holiday:
+            # Aktiv 10:00 - 22:00 -> Ruhe 22:00 - 10:00
+            start_quiet = datetime.strptime("22:00", "%H:%M").time()
+            end_quiet = datetime.strptime("10:00", "%H:%M").time()
+        else:
+            # Aktiv 08:00 - 22:00 -> Ruhe 22:00 - 08:00
+            start_quiet = datetime.strptime("22:00", "%H:%M").time()
+            end_quiet = datetime.strptime("08:00", "%H:%M").time()
 
-    async def set_pause(self, minutes=30):
-        self.pause_until = dt_util.now() + timedelta(minutes=minutes)
-        await self.async_request_refresh()
+        if start_quiet <= end_quiet:
+            return start_quiet <= now_time <= end_quiet
+        return now_time >= start_quiet or now_time <= end_quiet
+
+    async def _is_holiday(self):
+        holiday_cal = self.entry.data.get(CONF_HOLIDAY_CALENDAR)
+        if not holiday_cal: return False
+        now = dt_util.now()
+        # Fix für end_of_local_day:
+        start = now.replace(hour=0, minute=0, second=0, microsecond=0)
+        end = now.replace(hour=23, minute=59, second=59, microsecond=999)
+        try:
+            response = await self.hass.services.async_call("calendar", "get_events", {"entity_id": holiday_cal, "start_date_time": start, "end_date_time": end}, blocking=True, return_response=True)
+            events = response.get(holiday_cal, {}).get("events", [])
+            return len(events) > 0
+        except Exception: return False
+
+    async def _check_calendar_preheat(self, heat_up_mins):
+        cal_id = self.entry.data.get(CONF_POOL_CALENDAR)
+        if not cal_id or heat_up_mins <= 0: return False
+        now = dt_util.now()
+        try:
+            response = await self.hass.services.async_call("calendar", "get_events", {"entity_id": cal_id, "start_date_time": now, "end_date_time": now + timedelta(hours=24)}, blocking=True, return_response=True)
+            events = response.get(cal_id, {}).get("events", [])
+            for event in events:
+                event_start = dt_util.parse_datetime(event["start"])
+                if (event_start - timedelta(minutes=heat_up_mins)) <= now <= event_start: return True
+            return False
+        except Exception: return False
 
     def _get_float_state(self, entity_id):
         if not entity_id: return None
         state = self.hass.states.get(entity_id)
-        if state is None or state.state in ("unknown", "unavailable"): return None
-        try: return float(state.state)
+        try: return float(state.state) if state and state.state not in ("unknown", "unavailable") else None
         except ValueError: return None
-
-    async def _is_holiday(self):
-        """Prüft Feiertage über den Kalender-Service."""
-        holiday_cal = self.entry.data.get(CONF_HOLIDAY_CALENDAR)
-        if not holiday_cal: return False
-        
-        now = dt_util.now()
-        start = dt_util.start_of_local_day(now)
-        end = dt_util.end_of_local_day(now)
-        
-        try:
-            response = await self.hass.services.async_call(
-                "calendar", "get_events",
-                {"entity_id": holiday_cal, "start_date_time": start, "end_date_time": end},
-                blocking=True, return_response=True
-            )
-            events = response.get(holiday_cal, {}).get("events", [])
-            return len(events) > 0
-        except Exception:
-            return False
-
-    def _check_quiet_time(self, is_holiday):
-        now_time = dt_util.now().time()
-        start = datetime.strptime(self.entry.data.get(CONF_QUIET_START, "22:00"), "%H:%M").time()
-        end = datetime.strptime(self.entry.data.get(CONF_QUIET_END, "06:00"), "%H:%M").time()
-        if start <= end: return start <= now_time <= end
-        return now_time >= start or now_time <= end
 
     def calculate_heat_up_time(self, current_temp, power_watt):
         if current_temp is None or current_temp >= self.target_temp or power_watt <= 0: return 0
         volume = self.entry.data.get(CONF_WATER_VOLUME, 1000)
-        delta_t = self.target_temp - current_temp
-        hours = (volume * 1.16 * delta_t) / power_watt
-        return round(hours * 60)
-
-    async def _check_calendar_preheat(self, heat_up_mins):
-        """Prüft Pool-Events über den Kalender-Service."""
-        cal_id = self.entry.data.get(CONF_POOL_CALENDAR)
-        if not cal_id or heat_up_mins <= 0: return False
-        
-        now = dt_util.now()
-        start = now
-        end = now + timedelta(hours=24)
-        
-        try:
-            response = await self.hass.services.async_call(
-                "calendar", "get_events",
-                {"entity_id": cal_id, "start_date_time": start, "end_date_time": end},
-                blocking=True, return_response=True
-            )
-            events = response.get(cal_id, {}).get("events", [])
-            
-            for event in events:
-                event_start = dt_util.parse_datetime(event["start"])
-                if (event_start - timedelta(minutes=heat_up_mins)) <= now <= event_start:
-                    return True
-            return False
-        except Exception:
-            return False
-
-    def _check_filter_requirement(self):
-        return (dt_util.now() - self.last_filter_run) > timedelta(hours=12)
+        return round((volume * 1.16 * (self.target_temp - current_temp)) / power_watt * 60)
